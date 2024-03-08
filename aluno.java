a51acfefd608dea92b6f333f7c8587e5bb24bb2c
Bowie <marketing@letscode.com.br>
2024-03-07 22:08:14 -0300

    Primeiro comit

---
+++ aluno.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class aluno {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a matrícula:");
+
+    int matricula = scanner.nextInt();
+
+    System.out.print("Digite o nome do aluno: ");
+    scanner.nextLine(); // Consumir a quebra de linha pendente
+    String nome = scanner.nextLine();
+
+    System.out.println("Digite a nota 1: ");
+    int nota1 = scanner.nextInt();
+
+    System.out.println("Digite nota 2: ");
+    int nota2 = scanner.nextInt();
+
+    double notaFinal = (nota1 + nota2) / 2;
+    System.out.println("Resultado: " + notaFinal);
+
+    if (notaFinal >= 6) {
+      System.out.println("Aprovado");
+    } else if (notaFinal < 6) {
+      System.out.println("Reprovado");
+    }
+
+    System.out.println("Nome: " + nome);
+
+    scanner.close();
+  }
+}
