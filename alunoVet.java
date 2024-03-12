fd9124e2c8351490182adca982875287f356d89a
matheus99tur <matheusborba12@hotmail.com>
2024-03-11 22:50:14 -0300

    alterado

--- alunoVet.java
+++ alunoVet.java
@@ -3,32 +3,53 @@ import java.util.Scanner;
 public class alunoVet {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Digite a matrícula:");
 
-    int matricula = scanner.nextInt();
+    int vetorAluno = 5;
 
-    System.out.print("Digite o nome do aluno: ");
-    scanner.nextLine(); // Consumir a quebra de linha pendente
-    String nome = scanner.nextLine();
+    int [] matriculas = new int [vetorAluno];
+    String [] nomes = new String[vetorAluno];
+    double [] notas1 = new double[vetorAluno];
+    double [] notas2 = new double[vetorAluno];
+    double [] notasFinais = new double [vetorAluno];
+    
+    for (int i = 0; i < vetorAluno; i++) {
+      System.out.println("Aluno " + (i + 1));
+      System.out.print("Digite a matrícula do aluno: ");
+      matriculas[i] = scanner.nextInt();
 
-    System.out.println("Digite a nota 1: ");
-    int nota1 = scanner.nextInt();
+      System.out.print("Digite o nome do aluno: ");
+      scanner.nextLine(); // Consumindo a quebra de linha
+      nomes[i] = scanner.nextLine();
 
-    System.out.println("Digite nota 2: ");
-    int nota2 = scanner.nextInt();
+      System.out.print("Digite a nota 1 do aluno: ");
+      notas1[i] = scanner.nextDouble();
 
-    double notaFinal = (nota1 + nota2) / 2;
+      System.out.print("Digite a nota 2 do aluno: ");
+      notas2[i] = scanner.nextDouble();
 
-    if (notaFinal >= 6) {
-      System.out.println("Aprovado");
-    } else if (notaFinal < 6) {
-      System.out.println("Reprovado");
-    }
+      // Calculando a nota final
+      notasFinais[i] = (notas1[i] + notas2[i]) / 2;
 
-    System.out.println("Matricula: " + matricula);
-    System.out.println("Nome: " + nome);
-    System.out.println("Nota Final: " + notaFinal);
-    System.out.println("Resultado: " + notaFinal);
+      // Determinando o status do aluno
+      if (notasFinais[i] >= 6) {
+          status[i] = "Sim";
+      } else {
+          status[i] = "Não";
+      }
+  }
+
+  // Imprimindo o relatório final
+  System.out.println("\nRelatório Final:");
+  for (int i = 0; i < alunoVet; i++) {
+      System.out.println("Aluno " + (i + 1));
+      System.out.println("Matrícula: " + matriculas[i]);
+      System.out.println("Nome: " + nomes[i]);
+      System.out.println("Aprovado: (" + status[i] + ")");
+      System.out.println("Nota final: " + notasFinais[i] + "\n");
+  }
+
+
+
 
     scanner.close();
 
